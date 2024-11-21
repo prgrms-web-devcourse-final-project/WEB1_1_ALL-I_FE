5d48e48453ad5f6d72ef4b3bc0311127f5fdec37
wilderif <woojuro3@gmail.com>
2024-11-21 11:55:42 +0900

    chore: prettire.config.js 파일생성 및  설정

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+  endOfLine: "lf",
+};
+
+export default config;
