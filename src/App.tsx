2ee955907670d032b745b7871dba26b6319b57d5
wilderif <woojuro3@gmail.com>
2024-11-22 16:58:04 +0900

    feat: router 파일 작성
    - App.tsx에 router 파일 적용
    - 각 경로 테스트 발견한 버그들 예외처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 function App() {
-  return <div>Appppp</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
