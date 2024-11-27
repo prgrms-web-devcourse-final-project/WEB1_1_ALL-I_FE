5c6dd1890f643c8b47334973d1d8ae02f71a1123
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-11-27 10:19:13 +0900

    feat: 수정/삭제 모달 컴포넌트 제작
    - 이 컴포넌트는 수정/삭제 모달입니다. 위치는 절대 위치로 설정해야 합니다.
    - 수정/삭제 클릭시 실행할 함수를 전달하여야 합니다.

---
+++ src/components/common/EditDeleteModal/EditDeleteModal.style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Modal = styled.div<{
+  top?: number;
+  bottom?: number;
+  left?: number;
+  right?: number;
+}>`
+  position: absolute;
+  top: ${({ top }) => top}px;
+  bottom: ${({ bottom }) => bottom}px;
+  left: ${({ left }) => left}px;
+  right: ${({ right }) => right}px;
+  width: 72px;
+  height: 74px;
+  border-radius: var(--border-radius-large);
+  background-color: var(--color-white);
+  border: 1px solid var(--color-primary);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+`;
+
+export const Span = styled.span`
+  font-size: var(--font-size-small);
+  font-weight: var(--font-weight-bold);
+  font-family: "Noto Sans KR";
+  cursor: pointer;
+  &:first-child {
+    color: var(--color-gray-light);
+  }
+  &:last-child {
+    // 임시 컬러
+    color: red;
+  }
+`;
