702918d0ed9ceaf895c16b3611a1763737e0b954
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-12-01 21:29:18 +0900

    feat: useTodoScheduleForm에 그룹일 경우에 관한 코드 추가

--- src/hooks/useTodoScheduleForm.ts
+++ src/hooks/useTodoScheduleForm.ts
@@ -1,25 +1,36 @@
 import { useState } from "react";
-import { OptionType } from "@/types/select.types";
+import { GroupMember, OptionType } from "@/types/select.types";
 import { useToast } from "@/hooks/useToast";
 
 interface useTodoScheduleFormProps {
-  withEndDate?: boolean;
-  withEndTime?: boolean;
+  withEndDate?: boolean; // 종료일 입력 여부
+  withEndTime?: boolean; // 종료시간 입력 여부
+  withGroup?: boolean; // 그룹원 선택 여부
 }
 
 export const useTodoScheduleForm = ({
   withEndDate = false,
   withEndTime = false,
+  withGroup = false,
 }: useTodoScheduleFormProps = {}) => {
+  // 내용
   const [content, setContent] = useState<string>("");
+  // 카테고리
   const [categoryList, setCategoryList] = useState<OptionType[]>([]);
   const [category, setCategory] = useState<OptionType | null>(null);
+  // 멤버
+  const [memberList, setMemberList] = useState<GroupMember[]>([]);
+  const [member, setMember] = useState<GroupMember[]>([]);
+  // 날짜
   const [startDate, setStartDate] = useState<Date>(new Date());
   const [endDate, setEndDate] = useState<Date>(new Date());
+  // 시간
   const [startTime, setStartTime] = useState<string>("09:00");
   const [endTime, setEndTime] = useState<string>("10:00");
+  // 토글
   const [isTimeOn, setIsTimeOn] = useState<boolean>(false);
   const [isAlarmOn, setIsAlarmOn] = useState<boolean>(false);
+  // 토스트
   const { showToast } = useToast();
 
   const handleContentChange = (value: string) => {
@@ -28,6 +39,10 @@ export const useTodoScheduleForm = ({
 
   const handleCategoryChange = (value: OptionType | null) => {
     setCategory(value);
+  };
+
+  const handleMemberChange = (value: GroupMember[]) => {
+    setMember(value);
   };
 
   const handleDateChange = (newStartDate: Date, newEndDate?: Date) => {
@@ -58,6 +73,10 @@ export const useTodoScheduleForm = ({
       showToast("카테고리를 입력해주세요.", "error");
       ok = false;
     }
+    if (withGroup && member.length === 0) {
+      showToast("멤버를 선택해주세요.", "error");
+      ok = false;
+    }
     return ok;
   };
 
@@ -66,6 +85,10 @@ export const useTodoScheduleForm = ({
     setContent,
     categoryList,
     setCategoryList,
+    memberList,
+    setMemberList,
+    member,
+    setMember,
     category,
     setCategory,
     startDate,
@@ -82,6 +105,7 @@ export const useTodoScheduleForm = ({
     setIsAlarmOn,
     handleContentChange,
     handleCategoryChange,
+    handleMemberChange,
     handleDateChange,
     handleTimeChange,
     handleTimeToggle,
