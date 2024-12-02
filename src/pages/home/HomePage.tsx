9f4e17a82bfa6db409e44d8b08515ca9cb08a3c8
wilderif <woojuro3@gmail.com>
2024-12-02 10:48:32 +0900

    chore: 더미 투두 데이터 생성

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,12 @@
-import CalendarMonth from "@/components/common/Calendar/month/CalendarMonth";
+import Calendar from "@/components/common/Calendar/Calendar";
 
 function HomePage() {
   return (
     <div>
       <div>HomePage</div>
+      <div style={{ padding: "50px 0" }}>
+        <Calendar />
+      </div>
     </div>
   );
 }
