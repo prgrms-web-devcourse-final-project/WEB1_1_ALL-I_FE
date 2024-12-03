bd5e2ba7e039d5fb4779447d98393fd08b51cc9e
wilderif <woojuro3@gmail.com>
2024-12-03 10:21:12 +0900

    refactor: 홈 페이지 임시 컴포넌트 호출 부분 제거

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,7 @@
-import Calendar from "@/components/common/Calendar/Calendar";
-
 function HomePage() {
   return (
     <div>
       <div>HomePage</div>
-      <div style={{ padding: "50px 0" }}>
-        <Calendar />
-      </div>
     </div>
   );
 }
