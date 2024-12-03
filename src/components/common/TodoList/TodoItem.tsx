1bd80aa5227baf7465d7001a5c5342dc442196eb
Ga-Zero <rrtu123@naver.com>
2024-12-03 14:30:44 +0900

    refactor: 일정, 투두 컴포넌트

--- src/components/common/TodoList/TodoItem.tsx
+++ src/components/common/TodoList/TodoItem.tsx
@@ -1,53 +1,66 @@
 import * as Styled from "./TodoItem.style";
 import useTodoItem from "@/hooks/useTodoItem";
-import TextDate from "../TextDate/TextDate";
 import ListBar from "../ListBar/ListBar";
+import EditDeleteIcon from "@/components/feature/EditDeleteIcon/EditDeleteIcon";
+import { useNavigate } from "react-router-dom";
+import TextDate from "../TextDate/TextDate";
+import { FormatTime } from "@/utils/format";
 
-interface GroupMember {
-  name: string; // 이름
-  process: boolean; // todo 완료 상태
-}
 interface TodoItemProps {
-  values: string[];
-  color: string;
-  todo: string;
-  isComplete: boolean;
-  isMember: GroupMember[];
+  id: string; // ID
+  title: string; // 투두 제목
+  date: string; // 날짜
+  startTime?: string | null; // 시작 시간 (옵션)
+  done: boolean; // 완료 여부
+  categoryId: string; // 카테고리 ID
+  color: string; // 카테고리 색상
+  onDelete: (id: string) => void; // 삭제 핸들러
 }
+
 function TodoItem({
-  values = [],
-  color = "#EBFFE1",
-  todo = "기말고사기말고사기말고사기말기말고사기말고사기말고사기말",
-  isComplete = false,
-  isMember = [],
+  id,
+  title,
+  date,
+  startTime,
+  done,
+  categoryId,
+  color,
+  onDelete,
 }: TodoItemProps) {
-  const { isChecked, toggleChecked } = useTodoItem({ isComplete });
+  const { isChecked, toggleChecked } = useTodoItem({ isComplete: done });
+
+  const navigate = useNavigate();
+
+  // 수정 버튼 핸들러 (수정 페이지랑 연결되는거 나중에 수정해야함)
+  const handleEditClick = () => {
+    navigate(`/main/todo/${id}/edit`, {
+      state: { id, title, date, startTime, categoryId },
+    });
+  };
 
   return (
     <Styled.Wrapper>
+      {startTime && <TextDate values={[FormatTime(startTime)]} />}
       <Styled.ExplanContainer>
-        {values.length > 0 ? <TextDate values={values} /> : ""}
         <ListBar color={color} />
-        <Styled.TodoText isChecked={isChecked}>{todo}</Styled.TodoText>
+        <Styled.TodoText isChecked={isChecked}>{title}</Styled.TodoText>
       </Styled.ExplanContainer>
       <Styled.SetContainer>
-        {isMember.length ? (
-          <Styled.AssignWrapper>
-            {isMember.map((people: GroupMember) => (
-              <Styled.AssignPeople isMemDone={people.process}>
-                {people.name}{" "}
-              </Styled.AssignPeople>
-            ))}
-          </Styled.AssignWrapper>
-        ) : (
-          ""
-        )}
+        <Styled.AssignWrapper>
+          {/* 나중에 팀원 목록 쓰는 구간 */}
+          {/* <Styled.AssignPeople isMemDone={true}>이가영</Styled.AssignPeople> */}
+        </Styled.AssignWrapper>
+        {/* 투두 체크 박스 */}
         <Styled.CheckboxCustom
           type="checkbox"
           checked={isChecked}
           onChange={toggleChecked}
         />
-        <span>(아이콘)</span>
+        {/* 수정/삭제 모달 아이콘 */}
+        <EditDeleteIcon
+          onEdit={handleEditClick}
+          onDelete={() => onDelete(id)}
+        />
       </Styled.SetContainer>
     </Styled.Wrapper>
   );
