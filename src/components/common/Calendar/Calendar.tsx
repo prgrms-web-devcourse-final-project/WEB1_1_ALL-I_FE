1c39cbbf35d07d189d6e03d7925ecf197f3dfe43
wilderif <woojuro3@gmail.com>
2024-12-03 10:19:40 +0900

    refactor: 달력 투두 제거 되면서 필요 없는 코드 주석 처리

--- src/components/common/Calendar/Calendar.tsx
+++ src/components/common/Calendar/Calendar.tsx
@@ -25,14 +25,6 @@ const CATEGORY_COLOR = {
  * 카테고리 로직 나오면 해당 카테고리 색상 사용하도록 변경
  *
  * 페이지 작업 시 임시로 감싼 div 제거
- *
- * 투두 관련 로직 최적화 할것
- * - 여러번 확인하지 않도록
- * - 스타일 파일, 컴포넌트 파일 코드 정리
- * - 이벤트 마운트 부분 정리
- *
- * handleDateClick 함수 prop으로 받도록 변경
- *
  * 홈페이지에서 삭제
  */
 interface CalendarProps {
@@ -62,20 +54,9 @@ const Calendar = ({ onDateSelect }: CalendarProps) => {
     return formattedEvent;
   });
 
-  // 투두 map 함수
-  // done 관련 표시 로직 추가할 것
-  const todoEvents = PERSONAL_TODO_DATA.map((todo) => ({
-    id: todo.personalTodoId,
-    title: todo.title,
+  // 투두 날짜 map 함수
+  const todoEventsDate = PERSONAL_TODO_DATA.map((todo) => ({
     start: todo.date,
-    // 카테고리 로직 나오면 해당 카테고리 색상 사용하도록 변경
-    // display: "list-item", // 주간 뷰에서 리스트 형태로 표시
-    allDay: true,
-    backgroundColor:
-      CATEGORY_COLOR[todo.categoryId as keyof typeof CATEGORY_COLOR],
-    extendedProps: {
-      isTodo: true, // 투두 아이템 구분을 위한 플래그
-    },
   }));
 
   return (
@@ -86,134 +67,132 @@ const Calendar = ({ onDateSelect }: CalendarProps) => {
         alignItems: "center",
       }}
     >
-      <div style={{ width: "80%" }}>
-        <Styled.CalendarWrapper>
-          <FullCalendar
-            // 기본 설정
-            plugins={[dayGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-            locale="ko"
-            contentHeight="auto"
-            fixedWeekCount={false} // 고정된 6주 레이아웃 비활성화
-            // 헤더 설정
-            headerToolbar={{
-              left: "", // 왼쪽 비움
-              center: "prev title next", // 이전, 제목, 다음 버튼을 중앙에 모두 배치
-              right: "dayGridMonth,dayGridWeek", // 월간/주간 전환 버튼 추가
-            }}
-            buttonText={{
-              dayGridMonth: "월", // "Month"를 "월"로 변경
-              dayGridWeek: "주", // "Week"를 "주"로 변경
-            }}
-            dayHeaderFormat={{ weekday: "short" }} // 요일 표시 형식
-            // 이벤트 데이터 및 표시 설정
-            events={events}
-            /**
-             * 투두 리스 로직 후순위
-             */
-            // eventContent={(arg) => {
-            //   if (arg.event.extendedProps.isTodo) {
-            //     if (arg.view.type === "dayGridMonth") {
-            //       return { html: "" }; // 월간 뷰에서 숨김
-            //     }
-            //     if (arg.view.type === "dayGridWeek") {
-            //       return (
-            //         <div className="todo-item">
-            //           <input type="checkbox" className="todo-checkbox" />
-            //           <span>{arg.event.title}</span>
-            //         </div>
-            //       );
-            //     }
-            //   }
-            //   return { html: arg.event.title }; // 기본 이벤트
-            // }}
-            // eventContent 제거하고
-            eventDidMount={(arg) => {
-              // 월간 뷰에서는 투두 아이템을 점으로만 표시
-              if (arg.event.extendedProps.isTodo) {
-                if (arg.view.type === "dayGridMonth") {
-                  arg.el.style.display = "none";
-                }
-                // if (arg.view.type === "dayGridWeek") {
-                //   arg.el.style.display = "block";
-                //   arg.el.classList.add("todo-item");
-                //   // 체크박스 추가
-                //   const eventTitle = arg.el.querySelector(".fc-event-title");
-                //   if (eventTitle) {
-                //     const checkbox = document.createElement("input");
-                //     checkbox.type = "checkbox";
-                //     checkbox.className = "todo-checkbox";
-                //     eventTitle.insertBefore(checkbox, eventTitle.firstChild);
-                //   }
-                // }
-                // arg.el.classList.add("todo-item");
-                // // 체크박스 추가
-                // const eventTitle = arg.el.querySelector(".fc-event-title");
-                // if (eventTitle) {
-                //   const checkbox = document.createElement("input");
-                //   checkbox.type = "checkbox";
-                //   checkbox.className = "todo-checkbox";
-                //   eventTitle.insertBefore(checkbox, eventTitle.firstChild);
-                // }
-              }
-            }}
-            // 투두 관련 로직 최적화 할것
-            dayCellDidMount={(arg) => {
-              // 날짜를 YYYY-MM-DD 형식으로 변환 (로컬 시간 기준)
-              const currentDate = arg.date.toLocaleDateString("en-CA"); // en-CA는 YYYY-MM-DD 형식을 반환
+      <Styled.CalendarWrapper>
+        <FullCalendar
+          // 기본 설정
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          locale="ko"
+          contentHeight="auto"
+          fixedWeekCount={false} // 고정된 6주 레이아웃 비활성화
+          // 헤더 설정
+          headerToolbar={{
+            left: "", // 왼쪽 비움
+            center: "prev title next", // 이전, 제목, 다음 버튼을 중앙에 모두 배치
+            right: "dayGridMonth,dayGridWeek", // 월간/주간 전환 버튼 추가
+          }}
+          buttonText={{
+            dayGridMonth: "월", // "Month"를 "월"로 변경
+            dayGridWeek: "주", // "Week"를 "주"로 변경
+          }}
+          dayHeaderFormat={{ weekday: "short" }} // 요일 표시 형식
+          // 이벤트 데이터 및 표시 설정
+          events={events}
+          /**
+           * 투두 리스 로직 후순위
+           */
+          // eventContent={(arg) => {
+          //   if (arg.event.extendedProps.isTodo) {
+          //     if (arg.view.type === "dayGridMonth") {
+          //       return { html: "" }; // 월간 뷰에서 숨김
+          //     }
+          //     if (arg.view.type === "dayGridWeek") {
+          //       return (
+          //         <div className="todo-item">
+          //           <input type="checkbox" className="todo-checkbox" />
+          //           <span>{arg.event.title}</span>
+          //         </div>
+          //       );
+          //     }
+          //   }
+          //   return { html: arg.event.title }; // 기본 이벤트
+          // }}
+          // eventContent 제거하고
+          // eventDidMount={(arg) => {
+          //   // 월간 뷰에서는 투두 아이템을 점으로만 표시
+          //   if (arg.event.extendedProps.isTodo) {
+          //     if (arg.view.type === "dayGridMonth") {
+          //       arg.el.style.display = "none";
+          //     }
+          //     // if (arg.view.type === "dayGridWeek") {
+          //     //   arg.el.style.display = "block";
+          //     //   arg.el.classList.add("todo-item");
+          //     //   // 체크박스 추가
+          //     //   const eventTitle = arg.el.querySelector(".fc-event-title");
+          //     //   if (eventTitle) {
+          //     //     const checkbox = document.createElement("input");
+          //     //     checkbox.type = "checkbox";
+          //     //     checkbox.className = "todo-checkbox";
+          //     //     eventTitle.insertBefore(checkbox, eventTitle.firstChild);
+          //     //   }
+          //     // }
+          //     // arg.el.classList.add("todo-item");
+          //     // // 체크박스 추가
+          //     // const eventTitle = arg.el.querySelector(".fc-event-title");
+          //     // if (eventTitle) {
+          //     //   const checkbox = document.createElement("input");
+          //     //   checkbox.type = "checkbox";
+          //     //   checkbox.className = "todo-checkbox";
+          //     //   eventTitle.insertBefore(checkbox, eventTitle.firstChild);
+          //     // }
+          //   }
+          // }}
+          // 투두 관련 로직 최적화 할것
+          dayCellDidMount={(arg) => {
+            // 날짜를 YYYY-MM-DD 형식으로 변환 (로컬 시간 기준)
+            const currentDate = arg.date.toLocaleDateString("en-CA"); // en-CA는 YYYY-MM-DD 형식을 반환
 
-              // 해당 날짜에 투두가 있는지 확인
-              const hasTodo = todoEvents.some(
-                (todo) => todo.start === currentDate
-              );
+            // 해당 날짜에 투두가 있는지 확인
+            const hasTodo = todoEventsDate.some(
+              (todo) => todo.start === currentDate
+            );
 
-              // 투두가 있으면 점 표시 추가
-              if (hasTodo) {
-                const dot = document.createElement("div");
-                dot.className = "todo-dot";
-                arg.el.querySelector(".fc-daygrid-day-top")?.appendChild(dot);
-              }
-            }}
-            weekends={true}
-            eventDisplay="block"
-            eventBackgroundColor="var(--color-primary)"
-            eventBorderColor="transparent"
-            // 날짜 셀 설정
-            dayCellClassNames="calendar-day"
-            dayCellContent={({ date }) => {
-              return { html: date.getDate().toString() };
-            }}
-            // 이벤트 표시 제한 설정
-            dayMaxEvents={3}
-            moreLinkContent={({ num }) => `+${num}`} // 더보기 텍스트 커스터마이징
-            // 더보기 클릭 시 동작 커스터마이징
-            moreLinkClick={() => {
-              return "background";
-            }}
-            // 더보기 팝오버 헤더 포맷 설정
-            dayPopoverFormat={{
-              month: "long",
-              day: "numeric",
-              weekday: "long",
-            }}
-            // 사용 가능한 view 설정
-            views={{
-              // 주간 보기 커스터마이징
-              dayGridWeek: {
-                // 주간 보기 제목 형식
-                titleFormat: {
-                  year: "numeric",
-                  month: "long",
-                },
-                dayHeaderFormat: { weekday: "short" }, // 요일 표시 형식
-                dayMaxEvents: false,
+            // 투두가 있으면 점 표시 추가
+            if (hasTodo) {
+              const dot = document.createElement("div");
+              dot.className = "todo-dot";
+              arg.el.querySelector(".fc-daygrid-day-top")?.appendChild(dot);
+            }
+          }}
+          weekends={true}
+          eventDisplay="block"
+          eventBackgroundColor="var(--color-primary)"
+          eventBorderColor="transparent"
+          // 날짜 셀 설정
+          dayCellClassNames="calendar-day"
+          dayCellContent={({ date }) => {
+            return { html: date.getDate().toString() };
+          }}
+          // 이벤트 표시 제한 설정
+          dayMaxEvents={3}
+          moreLinkContent={({ num }) => `+${num}`} // 더보기 텍스트 커스터마이징
+          // 더보기 클릭 시 동작 커스터마이징
+          moreLinkClick={() => {
+            return "background";
+          }}
+          // 더보기 팝오버 헤더 포맷 설정
+          dayPopoverFormat={{
+            month: "long",
+            day: "numeric",
+            weekday: "long",
+          }}
+          // 사용 가능한 view 설정
+          views={{
+            // 주간 보기 커스터마이징
+            dayGridWeek: {
+              // 주간 보기 제목 형식
+              titleFormat: {
+                year: "numeric",
+                month: "long",
               },
-            }}
-            // 날짜 클릭 이벤트 추가
-            dateClick={handleDateClick}
-          />
-        </Styled.CalendarWrapper>
-      </div>
+              dayHeaderFormat: { weekday: "short" }, // 요일 표시 형식
+              dayMaxEvents: false,
+            },
+          }}
+          // 날짜 클릭 이벤트 추가
+          dateClick={handleDateClick}
+        />
+      </Styled.CalendarWrapper>
     </div>
   );
 };
