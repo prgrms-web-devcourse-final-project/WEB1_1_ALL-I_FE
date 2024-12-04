7c5fd376bae480355414adb87fa545571c7ca44e
wilderif <woojuro3@gmail.com>
2024-12-04 17:05:14 +0900

    feat: CategoryButtons 컴포넌트 폴더 이동 및 수정

--- src/components/feature/main/CategoryButtons/CategoryButtons.tsx
+++ src/components/feature/main/CategoryButtons/CategoryButtons.tsx
@@ -1,20 +1,25 @@
 import SettingIcon from "@/assets/icons/setting.svg?react";
 import FilterIcon from "@/assets/icons/filtering.svg?react";
-import * as Styled from "./MainCategoryButton.style";
+import * as Styled from "./CategoryButtons.style";
 import { useNavigate } from "react-router-dom";
 
 function MainCategoryButton() {
   const navigate = useNavigate();
-  const handleClick = () => {
+
+  const handleFilterClick = () => {
+    console.log("filter");
+  };
+  const handleSettingClick = () => {
     navigate("/categories");
   };
+
   return (
     <Styled.Container>
-      <Styled.Button onClick={handleClick}>
-        <SettingIcon width={20} height={20} fill="none" stroke="currentColor" />
-      </Styled.Button>
-      <Styled.Button>
+      <Styled.Button onClick={handleFilterClick}>
         <FilterIcon width={20} height={20} fill="none" stroke="currentColor" />
+      </Styled.Button>
+      <Styled.Button onClick={handleSettingClick}>
+        <SettingIcon width={20} height={20} fill="none" stroke="currentColor" />
       </Styled.Button>
     </Styled.Container>
   );
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,10 +1,12 @@
 import Calendar from "@/components/common/Calendar/Calendar";
 import ScheduleList from "@/components/feature/main/ScheduleList/ScheduleList";
+import CategoryButtons from "@/components/feature/main/CategoryButtons/CategoryButtons";
 
 function MainPage() {
   return (
     <>
       <Calendar usage="main" onDateSelect={() => {}} />
+      <CategoryButtons />
       <ScheduleList />
     </>
   );
