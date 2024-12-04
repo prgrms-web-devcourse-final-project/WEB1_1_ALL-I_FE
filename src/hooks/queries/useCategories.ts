25b5dd15415fa8631a1b6fbd192f605c753af7d3
wilderif <woojuro3@gmail.com>
2024-12-04 14:43:12 +0900

    feat: query 로직 나오기 전 컴포넌트 작업을 위하여, use categories, schedules, todos 파일 임시 작성

--- src/hooks/queries/useCategories.ts
+++ src/hooks/queries/useCategories.ts
@@ -2,3 +2,9 @@
  * 임시로 더미데이터 사용하고,
  * api 연동시 query 로직이 포함될 예정입니다.
  */
+
+import { CATEGORY_DATA } from "@/mocks/CTEGORY_DATA";
+
+export const useCategories = () => {
+  return { data: CATEGORY_DATA, isLoading: false };
+};
--- src/hooks/queries/useTodos.ts
+++ src/hooks/queries/useTodos.ts
@@ -2,3 +2,9 @@
  * 임시로 더미데이터 사용하고,
  * api 연동시 query 로직이 포함될 예정입니다.
  */
+
+import { PERSONAL_TODO_DATA } from "@/mocks/PERSONAL_TODO_DATA";
+
+export const useTodos = () => {
+  return { data: PERSONAL_TODO_DATA, isLoading: false };
+};
