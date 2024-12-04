9974274abbe2c136f4c414f1345f6c3844ec936e
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-12-04 19:29:59 +0900

    feat: API 인스턴스 생성
    - apiClient 생성
    - get, post, patch, delete 함수 생성
    - 예시 함수 생성

---
+++ src/apis/apiService.ts
@@ -0,0 +1,67 @@
+import axios, { AxiosInstance } from "axios";
+
+// Axios 인스턴스 생성
+const apiClient: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL, // 기본 URL 설정,
+  timeout: 3000, // 요청 타임아웃 설정
+  headers: { "Content-Type": "application/json" }, // 기본 헤더 설정
+});
+
+// 토큰 관련 인터셉터 추가
+apiClient.interceptors.request.use(
+  (config) => {
+    // 토큰 저장한 방식에 맞춰 아래 코드 변경하면 될 것 같습니다.
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// GET 요청 함수
+export const getRequest = async (url: string, params?: object) => {
+  try {
+    const response = await apiClient.get(url, { params });
+    return response.data;
+  } catch (error) {
+    console.error("GET 요청 에러:", error);
+    throw error;
+  }
+};
+
+// POST 요청 함수
+export const postRequest = async (url: string, data: object) => {
+  try {
+    const response = await apiClient.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("POST 요청 에러:", error);
+    throw error;
+  }
+};
+
+// PATCH 요청 함수
+export const updateData = async (url: string, data: object) => {
+  try {
+    const response = await apiClient.patch(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("PATCH 요청 에러:", error);
+    throw error;
+  }
+};
+
+// DELETE 요청 함수
+export const deleteData = async (url: string, params?: object) => {
+  try {
+    const response = await apiClient.delete(url, { params });
+    return response.data;
+  } catch (error) {
+    console.error("DELETE 요청 에러:", error);
+    throw error;
+  }
+};
--- src/hooks/queries/usePersonalTodos.ts
+++ src/hooks/queries/usePersonalTodos.ts
@@ -1,10 +1,13 @@
-/**
- * 임시로 더미데이터 사용하고,
- * api 연동시 query 로직이 포함될 예정입니다.
- */
+import { useQuery } from "@tanstack/react-query";
+import { getPersonalTodos } from "@/apis/todoAPI";
+import { DateRequestParams } from "@/types/todo.types";
 
-import { PERSONAL_TODO_DATA } from "@/mocks/PERSONAL_TODO_DATA";
+// 예시용 특정 달의 투두 조회 query
+export const usePersonalTodos = (dateParams: DateRequestParams) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos", dateParams.year, dateParams.month],
+    queryFn: () => getPersonalTodos(dateParams),
+  });
 
-export const usePersonalTodos = () => {
-  return { data: PERSONAL_TODO_DATA, isLoading: false, error: null };
+  return { data, isLoading, error };
 };
