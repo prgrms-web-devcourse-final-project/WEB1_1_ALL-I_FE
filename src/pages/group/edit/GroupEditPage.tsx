717d2c50b2828fd5e6cc182632488a19b0ce6317
JunYoung <whwnsdud011@naver.com>
2024-12-04 09:42:55 +0900

    fix: routes파일 groupEdit  오류 수정

--- src/pages/group/edit/GroupEditPage.tsx
+++ src/pages/group/edit/GroupEditPage.tsx
@@ -5,11 +5,12 @@ import MemberList from "@/components/feature/GroupNewPage/MemberList";
 import useGroupEditForm from "@/hooks/useGroupEditForm";
 
 interface GroupEditPageProps {
-  groupName: string;
-  color: string;
+  groupName?: string;
+  color?: string;
 }
 
 // 수정 페이지에서는 color랑 description 만 변경됨.
+// 데이터를 props를 통해 받게 되어있는데, useParam or useLocation에 맞춰 추후 수정 필요
 function GroupEditPage({
   groupName = "DBDB DEEP",
   color = "#c9c9c9",
