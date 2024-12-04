862d7907e982bf93f77b0d082cd541f4222eadbb
wilderif <woojuro3@gmail.com>
2024-12-04 11:46:08 +0900

    feat: navConfig 파일 수정

--- src/components/layout/NavBar/navConfig.ts
+++ src/components/layout/NavBar/navConfig.ts
@@ -3,13 +3,36 @@ import GroupIcon from "@/assets/icons/group.svg?react";
 import ChatbotIcon from "@/assets/icons/chatbot.svg?react";
 import ProfileIcon from "@/assets/icons/profile.svg?react";
 
+/**
+ * 네비게이션 바 있는 경로
+ *
+ * /main, /group, /group/:groupId, /chat, /notifications, /mypage
+ */
+type NavPath = {
+  pattern: string;
+  excludes?: string[];
+};
+
+export const SHOW_NAV: NavPath[] = [
+  { pattern: "/main" },
+  { pattern: "/group" },
+  {
+    pattern: "/group/:groupId",
+    excludes: [
+      "/group/new",
+      "/group/:groupId/edit",
+      "/group/:groupId/todo/*",
+      "/group/:groupId/schedule/*",
+    ],
+  },
+  { pattern: "/chat" },
+  { pattern: "/notifications" },
+  { pattern: "/mypage", excludes: ["/mypage/edit"] },
+];
+
 export const NAV_ITEMS = [
   { path: "/main", icon: HomeIcon, label: "메인" },
   { path: "/group", icon: GroupIcon, label: "그룹" },
   { path: "/chat", icon: ChatbotIcon, label: "챗봇" },
   { path: "/mypage", icon: ProfileIcon, label: "프로필" },
 ] as const;
-
-export const HIDDEN_PATHS = ["/signup", "/group/*"] as const;
-
-// 메인, 그룹/아이디, 챗봇, 프로필
