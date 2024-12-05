2ef1fe9a638f7e6c5df40151dee7fcedc3aee14e
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-12-05 15:04:42 +0900

    chore: eslint 설정 변경

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
 
       "react-refresh/only-export-components": [
         "warn",
