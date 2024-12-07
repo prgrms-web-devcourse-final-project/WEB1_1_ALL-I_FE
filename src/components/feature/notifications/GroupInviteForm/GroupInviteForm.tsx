f74cbd2762cf6b79327e49fa2296f86a2a247a75
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-12-07 23:19:29 +0900

    refactor: 알림 페이지에 쓰이는 공통 컴포넌트 수정

--- src/components/feature/notifications/GroupInviteForm/GroupInviteForm.tsx
+++ src/components/feature/notifications/GroupInviteForm/GroupInviteForm.tsx
@@ -1,33 +1,72 @@
 import { Button } from "@/components/common/Button/Button.style";
 import * as Styled from "./GroupInviteForm.style";
+import {
+  acceptGroupInvitation,
+  getUserNameById,
+  rejectGroupInvitation,
+} from "@/apis/alarm";
+import { useEffect, useState } from "react";
 
 interface GroupInviteFormProps {
-  groupId: string;
-  name: string;
+  groupInvitationId: string;
+  senderId: string;
   groupName: string;
+  index: number;
+  removeNotification: (index: number) => void;
 }
 
-function GroupInviteForm({ groupId, name, groupName }: GroupInviteFormProps) {
-  const handleNo = () => {
-    console.log("거절", groupId);
+function GroupInviteForm({
+  groupInvitationId,
+  senderId,
+  groupName,
+  index,
+  removeNotification,
+}: GroupInviteFormProps) {
+  const [senderName, setSenderName] = useState(""); // 보낸 사람 이름
+
+  // 그룹 초대 수락
+  const handleAccept = async () => {
+    try {
+      await acceptGroupInvitation(groupInvitationId);
+      removeNotification(index);
+    } catch (error) {
+      console.error("그룹 초대 수락 실패:", error);
+    }
   };
-  const handleYes = () => {
-    console.log("수락", groupId);
+
+  // 그룹 초대 거절
+  const handleReject = async () => {
+    try {
+      await rejectGroupInvitation(groupInvitationId);
+      removeNotification(index);
+    } catch (error) {
+      console.error("그룹 초대 수락 실패:", error);
+    }
   };
+
+  // senderId로 이름 조회
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getUserNameById(senderId);
+      setSenderName(data.data);
+    };
+
+    fetchData();
+  }, [senderId]);
 
   return (
     <Styled.Container>
       <Styled.Content>
-        <span>{name}</span>
+        <span>{senderName}</span>
         님이
         <span>{groupName}</span>
         그룹에 초대하였습니다.
       </Styled.Content>
       <Styled.Buttons>
-        <Button $buttonType="whiteSmall" onClick={handleNo}>
+        <Button $buttonType="whiteSmall" onClick={handleReject}>
           거절
         </Button>
-        <Button $buttonType="primarySmall" onClick={handleYes}>
+        <Button $buttonType="primarySmall" onClick={handleAccept}>
           수락
         </Button>
       </Styled.Buttons>
