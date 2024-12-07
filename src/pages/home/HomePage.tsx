cac43f8d8d3587199881470466adde015eed5f38
JunYoung <whwnsdud011@naver.com>
2024-12-08 00:50:12 +0900

    design: 온보딩 페이지 퍼블리싱

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,8 +1,26 @@
+import * as Styled from "./HomePage.style";
+import { useNavigate } from "react-router-dom";
+import Blub from "@/assets/icons/bulb.svg?react";
+import logo from "@/assets/onboard/logo.png";
+
 function HomePage() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div>HomePage</div>
-    </div>
+    <Styled.Wrapper onClick={() => navigate("/login")}>
+      <Styled.Container>
+        <Styled.IconContainer>
+          <Blub />
+          <Styled.ImgContainer>
+            <img src={logo} alt="J-AI" />
+          </Styled.ImgContainer>
+        </Styled.IconContainer>
+        <Styled.explan>
+          당신의 손안에서 만나는 스마트 비서 <br />
+          간편한 일정 관리의 시작
+        </Styled.explan>
+      </Styled.Container>
+    </Styled.Wrapper>
   );
 }
 
