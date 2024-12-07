02af4952d1bdf9f5b0ac3e798c3050c328bc3694
JunYoung <whwnsdud011@naver.com>
2024-12-07 13:03:53 +0900

    feat: 그룹 수정 api 연결

--- src/hooks/useGroupEditForm.ts
+++ src/hooks/useGroupEditForm.ts
@@ -1,11 +1,17 @@
 import { useState } from "react";
+import { putGroup } from "@/apis/group/putGroup";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 interface UseGroupEditProps {
+  groupName: string;
   initialColor: string;
   description: string;
+  groupId: string;
 }
 
 function useGroupEditForm(initialState: UseGroupEditProps) {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState(initialState);
 
   const handleChange = (name: keyof UseGroupEditProps) => (value: string) => {
@@ -15,9 +21,15 @@ function useGroupEditForm(initialState: UseGroupEditProps) {
     }));
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(formData);
+    const res = await putGroup({ formData });
+    if (res.code == 200) {
+      // 토스트 메세지를 띄우지 않는 이유는 navigate시 변경되어 있기 때문
+      navigate("/group/1");
+    } else {
+      toast.error("잠시후 다시 시도해주세요.");
+    }
   };
 
   return {
