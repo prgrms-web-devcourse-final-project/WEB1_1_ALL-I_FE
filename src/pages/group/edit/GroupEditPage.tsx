44f221456d92dd9fe6ba35c220cc189f25f817a6
JunYoung <whwnsdud011@naver.com>
2024-12-08 20:14:41 +0900

    feat: 그룹 수정 데이터 동적 바인딩 및 수정시 에러 토스트 메세지 추가

--- src/pages/group/edit/GroupEditPage.tsx
+++ src/pages/group/edit/GroupEditPage.tsx
@@ -1,30 +1,23 @@
 import * as Styled from "./GroupEditPage.style";
+import { useLocation } from "react-router-dom";
 import Button from "@/components/common/Button/Button";
 import CircleInput from "@/components/common/Circle/CircleInput";
 import MemberList from "@/components/feature/GroupNewPage/MemberList";
 import TextInput from "@/components/common/TextInput/TextInput";
 import useGroupEditForm from "@/hooks/useGroupEditForm";
 
-interface GroupEditPageProps {
-  groupName?: string;
-  color?: string;
-  description?: string;
-}
-
 // 수정 페이지에서는 color랑 description 만 변경됨.
 // 데이터를 props를 통해 받게 되어있는데, useParam or useLocation에 맞춰 추후 수정 필요
 
 // **가장 중요** -> 그룹 수정 api 연동해야 함
-function GroupEditPage({
-  groupName = "DBDB DEEP",
-  color = "#c9c9c9",
-  description = "설명설명~",
-}: GroupEditPageProps) {
+function GroupEditPage() {
+  const location = useLocation();
+  console.log(location.state); //
   const { formData, handleChange, handleSubmit } = useGroupEditForm({
-    groupName,
-    initialColor: color,
-    description,
-    groupId: "a85e5db7-593f-429a-bc80-385408f0b934",
+    groupName: location.state.groupName || "",
+    initialColor: location.state.color || "#FFFFFF",
+    description: location.state.description || "",
+    groupId: location.state.groupId,
   });
 
   return (
@@ -48,7 +41,7 @@ function GroupEditPage({
         value={formData.description}
         onChange={handleChange("description")}
       />
-      <MemberList />
+      <MemberList groupId={location.state.groupId} />
       <CircleInput
         defaultColor={formData.initialColor}
         onChange={(value) => handleChange("initialColor")(value)}
